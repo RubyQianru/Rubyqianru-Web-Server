73b56a73f5e6b33a15b393fd5687b8f89acdf25e
Qianru Zhang <142470034+RubyQianru@users.noreply.github.com>
2024-10-07 19:20:49 -0400

    Remove config

--- server/core/config.py
+++
@@ -1,9 +0,0 @@
-'''
-Contains configuration settings for your application.
-'''
-# server/core/config.py
-
-class Settings:
-    DB_URL: str = "postgresql://ruzhang:RZcp1207@localhost/job"
-
-settings = Settings()
