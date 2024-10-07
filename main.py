4653f8f87cb1d320f69ffa1ac3fa738b6ce81174
RubyQianru <qz2432@nyu.edu>
2024-10-07 20:51:58 +0000

    💥Update readme and main

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-
+# app/main.py
 from fastapi import FastAPI
-from typing import List
-from pydantic import BaseModel
-from postgres_db import books, database
+from server.api.v1.api import api_router
+from server.db.database import database
 
 app = FastAPI()
-
-class BookCreate(BaseModel):
-    title: str
-    author: str
-    price: float
-
-class BookResponse(BaseModel):
-    id: int
-    title: str
-    author: str
-    price: float
-
-class Config:
-  orm_mode=True
 
 @app.on_event("startup")
 async def startup():
@@ -28,16 +13,4 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-@app.post("/books/", response_model=BookResponse)
-async def create_book(book: BookCreate):
-    query = books.insert().values(title=book.title, author=book.author, price=book.price)
-    last_book_id = await database.execute(query)
-
-    query = books.select().where(books.c.id == last_book_id)
-    inserted_book = await database.fetch_one(query)
-    return inserted_book
-
-@app.get("/books/", response_model=List[BookResponse])
-async def get_books():
-    query = books.select()
-    return await database.fetch_all(query)
+app.include_router(api_router, prefix="/api/v1")
